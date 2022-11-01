13fa48b217d279318ecdec0121137d3df4e0ee33
makibishu <makibishu39@gmail.com>
2022-11-02 01:48:04 +0900

    メモリ消費量を若干削減（まだ実用レベルじゃない）

--- by_brute_force.py
+++ by_brute_force.py
@@ -1,13 +1,17 @@
+import functools
+
 ODDS = [0.3, 0.25, 0.2, 0.15, 0.1]
-GOAL = 3
+GOAL = 4
 
 class Node:
-    ID = 0
+    __slots__ = 'ID', 'probability', 'scores', 'children'
+
+    count = 0
     probabilities = [0] * len(ODDS)
 
     def __init__(self, parent: 'Node', index: int) -> None:
-        Node.ID += 1
-        self.ID = Node.ID
+        Node.count += 1
+        self.ID = Node.count
 
         self.probability = parent.probability * ODDS[index]
         self.scores = parent.scores.copy()
@@ -20,11 +24,17 @@ class Node:
             Node.probabilities[index] += self.probability
 
             print(self)
+
+            if index == (len(ODDS)-1):
+                del parent
+            del self
         
     def __repr__(self) -> str:
         return f'Node{self.ID}: {self.probability*100: .10f}%, {self.scores}'
 
 class Root(Node):
+    __slots__ = 'ID', 'probability', 'scores', 'children'
+
     def __init__(self):
         self.ID = 0
         self.probability = 1
